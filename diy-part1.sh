4010657d12a79ce7836ac6795eefbd06c3f6f0fe
Weitian <31085999+weitian8@users.noreply.github.com>
2021-01-28 17:27:15 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,4 +13,6 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 #sed -i 's@coolsnowwolf/packages@P3TERX/packages@' feeds.conf.default
 
-sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+
+#sed -i '$a src-git helloworld https://github.com/P3TERX/helloworld' feeds.conf.default
